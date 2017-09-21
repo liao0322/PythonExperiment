3ae77bc4a727aa75c7763aea51727e707e036f54
liaoxf <liao0322@hotmail.com>
2017-09-21 17:55:57 +0800

    nothing

--- python3/guest/guest/urls.py
+++ python3/guest/guest/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from sign import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
+    url(r'^index/$', views.index), # 添加index/路径配置
+    url(r'^login_action/$', views.login_action),
+    url(r'^event_manage/$', views.event_manage),
 ]
