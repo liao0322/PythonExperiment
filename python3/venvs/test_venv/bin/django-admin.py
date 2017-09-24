a85160075afee5e88d3c4ba6789cb1a78210d790
liaoxf <liao0322@hotmail.com>
2017-09-24 17:55:35 +0800

    nothing

---
+++ python3/venvs/test_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/liaoxf/Desktop/PythonExperiment/python3/venvs/test_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
